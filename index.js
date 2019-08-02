3b949cb24c64c9a847b9231db88a31c9a84ecb9e
Alex Loney <seceras@gmail.com>
2019-08-02 11:02:23 -0700

    Added enable/disable options for users from Tech Support and administoratrs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 let token = require('./token.json');
 var botconfig = require('./botconfig.json');
 
+let enableBot = true;
+
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
@@ -24,8 +26,48 @@ client.on('message', message => {
     if (message.channel.name !== botconfig.channel) return;
 
     // Check the message sent to see if it's the command we're looking for
-    if (message.content === `${botconfig.prefix}${botconfig.acceptcommand}`)
+    if (message.content === `${botconfig.prefix}enable` ||
+        message.content === `${botconfig.prefix}disable`)
     {
+        // Search through the member list for this specific user to check permissions
+        let member = message.guild.members
+                        .get(message.author.id);
+        if (!member)
+        {
+            console.log('Failed when attempting to look up user');
+            return;
+        }
+
+        // If the user is an admin or tech support, let them enable/disable the bot.
+        // otherwise prevent it with a message informing them that they do not have
+        // permission to do this. A complete list of permissions may be found here:
+        // https://discord.js.org/#/docs/main/stable/class/Permissions
+        if (member.hasPermission('ADMINISTRATOR') ||
+            member.roles.find(role => role.name === 'Tech Support'))
+        {
+            enableBot = true;
+            if (message.content.includes('enable')) enableBot = true;
+            else if (message.content.includes('disable')) enableBot = false;
+        }
+        else
+        {
+            message.author
+                    .send(
+                        'You do not have permission to execute that command.'
+                    )
+                    .catch(console.warn)
+        }
+
+        // In either case, delete the message that started this.
+        message
+            .delete()
+            .catch(console.warn);
+    }
+    else if (message.content === `${botconfig.prefix}${botconfig.acceptcommand}`)
+    {
+        // If the bot is not enabled, simply return and don't do anything
+        if (!enableBot) return;
+
         // Locate the role from the server that we'll be assigning (we need the ID)
         let acceptedRole = message.guild.roles.find(role => role.name === botconfig.role);
         if (acceptedRole === null)
@@ -47,6 +89,9 @@ client.on('message', message => {
     }
     else
     {
+        // If the bot is not enabled, simply return and don't do anything
+        if (!enableBot) return;
+
         // Remove the users message and send them a private message to inform
         // them that they should not send anything else to this channel.
         message
